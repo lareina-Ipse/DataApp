5d792fe18edf898a6639882478a81fe62fd133e2
lareina-Ipse <lareina.ipse@gmail.com>
2020-02-19 14:59:32 +0900

    EnvRoad Finish

--- app/src/main/java/kr/co/chience/dataapp/util/LogUtil.java
+++ app/src/main/java/kr/co/chience/dataapp/util/LogUtil.java
@@ -5,18 +5,26 @@ import android.util.Log;
 public class LogUtil {
 
     public static void e(String tag, String log) {
+
         Log.e(tag, "### " + log);
+
     }
 
     public static void w(String tag, String log) {
+
         Log.e(tag, "### " + log);
     }
 
+
     public static void d(String tag, String log) {
+
         Log.e(tag, "### " + log);
+
     }
 
     public static void i(String tag, String log) {
+
         Log.e(tag, "### " + log);
+
     }
 }
