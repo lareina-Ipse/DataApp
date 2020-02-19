c2f2bbe0189232d88d0772e1472820a52c055503
Ipse <ipse.alice@chience.co.kr>
2020-02-19 11:07:40 +0900

    EnvRoad

---
+++ app/src/main/java/kr/co/chience/dataapp/base/BaseInterface.java
@@ -0,0 +1,10 @@
+package kr.co.chience.dataapp.base;
+
+public interface BaseInterface {
+
+    public void initViews();
+    public void initListener();
+    public void initItems();
+    public void initProcess();
+
+}
